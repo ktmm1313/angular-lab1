5b7106e9e12435e8c25b2e0c91140f280f2bd994
katie <kvancleve13@yahoo.com>
2021-01-06 21:13:55 -0500

    starting files for angular lab 1

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
